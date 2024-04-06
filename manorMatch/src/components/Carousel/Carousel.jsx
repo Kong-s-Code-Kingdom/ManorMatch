542019850c67244486ba1f45c633695a845feb98
chengtchris1 <152359315+chengtchris1@users.noreply.github.com>
2024-04-06 15:36:54 -0700

    Added an extra row

--- manorMatch/src/components/Carousel/Carousel.jsx
+++ manorMatch/src/components/Carousel/Carousel.jsx
@@ -2,11 +2,18 @@ import React from 'react';
 import CarouselTile from './CarouselTile.jsx';
 const Carousel = () => {
   return (
-    <div className="flex flex-row">
-      <CarouselTile imageUrl="https://via.placeholder.com/150" />
-      <CarouselTile imageUrl="https://via.placeholder.com/150" />
-      <CarouselTile imageUrl="https://via.placeholder.com/150" />
-    </div>
+    <>
+      <div className="flex flex-row">
+        <CarouselTile imageUrl="https://fakeimg.pl/200x250/b84d4d/909090" />
+        <CarouselTile imageUrl="https://fakeimg.pl/200x250/b8a0a0/909090" />
+        <CarouselTile imageUrl="https://fakeimg.pl/200x250/b84d4d/909090" />
+      </div>
+      <div className="flex flex-row">
+        <CarouselTile imageUrl="https://fakeimg.pl/200x250/b8a0a0/909090" />
+        <CarouselTile imageUrl="https://fakeimg.pl/200x250/b84d4d/909090" />
+        <CarouselTile imageUrl="https://fakeimg.pl/200x250/b8a0a0/909090" />
+      </div>
+    </>
   );
 };
 
--- manorMatch/src/components/Carousel/CarouselTile.jsx
+++ manorMatch/src/components/Carousel/CarouselTile.jsx
@@ -4,7 +4,7 @@ const CarouselTile = ({ imageUrl }) => {
   return (
     <div className="relative">
       <img src={imageUrl} alt="Carousel Image" className="w-full" />
-      <button className="absolute bottom-4 right-4 bg-blue-900 text-white px-4 py-2 rounded">
+      <button className="absolute bottom-2 right-2 bg-blue-900 text-white px-4 py-2 rounded">
         Button
       </button>
     </div>
